28f8393e6361a216f3706f5324cc29ca09c91c16
Sarah Calbez <s.cavalcante@me.com>
2022-07-07 17:41:13 -0400

    feat:[UNIT-02]-Creating global styles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import BankHeader from "./Components/BankHeader";
 import Container from "./Components/Container";
+import { GlobalStyle } from "./Components/GlobalStyle";
 
 function App() {
   return (
     <>
+      <GlobalStyle/>
       <BankHeader />
       <Container />
     </>
